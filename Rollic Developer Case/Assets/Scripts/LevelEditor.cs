a890373a54c8e2a5313ab3f539c29e74d1bb5047
kcaybasi <k.furkancaybasi@gmail.com>
2022-09-15 16:08:10 +0300

    1.0.20-Obstacles added.

--- Rollic Developer Case/Assets/Scripts/LevelEditor.cs
+++ Rollic Developer Case/Assets/Scripts/LevelEditor.cs
@@ -1,36 +1,11 @@
-﻿
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.SceneManagement;
-using UnityEngine.SceneManagement;
 
-public class LevelEditor : EditorWindow
+public class LevelEditor : MonoBehaviour
 {
-    GameObject levelTemplate;
-    string userString;
-
-    [MenuItem("Window/Level Editor")]
-    public static void ShowWindow()
-    {
-        GetWindow<LevelEditor>();
-    }
-
-    private void OnGUI()
-    {
-       // GUILayout.Label("This is a level editor", EditorStyles.boldLabel);
-
-        userString = EditorGUILayout.TextField("Level Name", userString);
-
-        if(GUILayout.Button("Generate Level"))
-        {
-            var newscene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene);
-            newscene.name = userString;
-
-            levelTemplate = Resources.Load("LevelTemplate") as GameObject;
-            Instantiate(levelTemplate);
-
-            EditorSceneManager.SaveScene(newscene, userString+".unity");
-            
-        }
-    }
+    [SerializeField] List<GameObject> _counterList = new List<GameObject>();
+    [SerializeField] List<GameObject> _collectibleList = new List<GameObject>();
+    [SerializeField] List<GameObject> _obstacleList = new List<GameObject>();
 }
